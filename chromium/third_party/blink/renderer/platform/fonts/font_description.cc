7648a4c0b9bae2b7a6a6c800b8ffc908f6f6cea9
Dmitriy Kuminov <coding@dmik.org>
2020-06-13 00:51:36 +0400

    chromium: Fix small OS/2 typo.
    Regression of 8d57324.

--- chromium/third_party/blink/renderer/platform/fonts/font_description.cc
+++ chromium/third_party/blink/renderer/platform/fonts/font_description.cc
@@ -223,7 +223,7 @@ FontCacheKey FontDescription::CacheKey(
       static_cast<unsigned>(fields_.orientation_) << 1 |       // bit 2-3
       static_cast<unsigned>(fields_.subpixel_text_position_);  // bit 1
 
-#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_OS2) || defined(OS_OS2)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_OS2)
   float device_scale_factor_for_key = FontCache::DeviceScaleFactor();
 #else
   float device_scale_factor_for_key = 1.0f;
