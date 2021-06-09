fe975e1774dd1f166daa6ef402c7f950a8c2461a
Dmitriy Kuminov <coding@dmik.org>
2021-06-10 01:36:29 +0300

    chromium: v8: Fix endless spin in TimeTicks::IsHighResolution on OS/2.
    Appears to be an upstream bug.
    
    Closes #39.

--- chromium/v8/src/base/platform/time.cc
+++ chromium/v8/src/base/platform/time.cc
@@ -64,7 +64,7 @@ int64_t ComputeThreadTicks() {
 // _POSIX_MONOTONIC_CLOCK to -1.
 V8_INLINE int64_t ClockNow(clockid_t clk_id) {
 #if (defined(_POSIX_MONOTONIC_CLOCK) && _POSIX_MONOTONIC_CLOCK >= 0) || \
-  defined(V8_OS_BSD) || defined(V8_OS_ANDROID)
+  defined(V8_OS_BSD) || defined(V8_OS_ANDROID) || defined(V8_OS_OS2)
 // On AIX clock_gettime for CLOCK_THREAD_CPUTIME_ID outputs time with
 // resolution of 10ms. thread_cputime API provides the time in ns
 #if defined(V8_OS_AIX)
